2ac4a07432cbb8364cb3729c6b8ad7a1b41b7e5d
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-02-23 16:02:09 +0100

    Update email recipient in process.php

---
+++ process.php
@@ -0,0 +1,25 @@
+<?php
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+  $EmailTo = "francisakpali9@gmail.com"; 
+  $Subject = "Portfolio CV/Resume";
+  // prepare email body text
+  $Body .= "Name: ";
+  $Body .= $name;
+  $Body .= "\n"; 
+  $Body .= "Email: ";
+  $Body .= $email;
+  $Body .= "\n";
+  $Body .= "Message: ";
+  $Body .= $message;
+  $Body .= "\n";
+  // send email
+  $success = mail($EmailTo, $Subject, $Body, "From:".$email);
+  // redirect to success page
+  if ($success){
+    echo "success";
+  }else{
+    echo "invalid";
+  } 
+?>
